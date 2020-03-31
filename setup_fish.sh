f2d2266a906d80f39035e9b62c87f192281625a1
Jack Hall <jackwhall7@gmail.com>
2020-03-30 22:04:26 -0500

    fixed bad bash conditionals

--- setup_fish.sh
+++ setup_fish.sh
@@ -7,19 +7,19 @@ source $DIR/common.sh
 require conda
 require fish
 
-if [ chsh -s `which fish` ]; then
+if chsh -s `which fish`; then
     echo "changed shell to fish (must log out and in again to take effect)"
 else
     echo "could not set fish as the default shell"
 fi
 
-if [ ln --symbolic --force $DIR/fish ~/.config ]; then
+if ln --symbolic --force $DIR/fish ~/.config; then
     echo "created link to fish config in ~/.config/"
 else
     echo "could not configure fish"
 fi
 
-if [ ln --symbolic --no-target-directory $DIR/inputrc ~/.inputrc ]; then
+if ln --symbolic --no-target-directory $DIR/inputrc ~/.inputrc; then
     echo "created link to inputrc in home folder"
 else
     echo "could not set vi-mode in shell"
